d2e3321caa6399963e72b280ba0a866857196cbf
Marc César Forte <99797041+marc-cesar@users.noreply.github.com>
2024-03-25 17:24:45 +0100

    Update nav-bar.component.ts

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -8,7 +8,7 @@ import { RouterModule } from '@angular/router';
   template: `
 
 <nav class="bg-white border-gray-200 dark:bg-gray-900">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-10 py-4">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-2 sm:px-10 py-4">
     <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
         <img src="../../assets/bank-logo.svg" class="h-8" alt="MLOps banking system logo" />
         <span class="sm:inline self-center sm:text-2xl font-semibold whitespace-nowrap dark:text-white">MLOps Credit Prediction System</span>
