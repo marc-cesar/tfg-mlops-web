75e9281e727826de2b93c395c88aa38858522f12
Marc César Forte <99797041+marc-cesar@users.noreply.github.com>
2024-03-25 22:26:42 +0100

    Hash location

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import routeConfig from './app/routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routeConfig)
+    provideRouter(routeConfig, withHashLocation())
   ]
 })
   .catch((err) => console.error(err));
