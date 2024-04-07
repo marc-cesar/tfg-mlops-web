9bc37c82a45d8e1c1d2547dbee4590fa41783ffe
Marc César Forte <99797041+marc-cesar@users.noreply.github.com>
2024-04-08 01:49:51 +0200

    Check access for New Request and previous reccommendations

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -6,6 +6,27 @@ import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
+// Factory function to create a canActivate function with isAdmin check
+function createCheckTokenAccess(isAdminRequired: boolean = false) {
+    return (): boolean => {
+        let hasAccess = false;
+        const userStr = localStorage.getItem('currentUser');
+        if (userStr != null) {
+            const user = JSON.parse(userStr);
+            // Check if token exists and, if isAdminRequired, also check if user isAdmin
+            if (user.token && (!isAdminRequired || user.isAdmin)) {
+                hasAccess = true;
+            }
+        }
+
+        if (hasAccess) {
+            return true;
+        } else {
+            window.location.href = '#/Login'; // Redirect for unauthorized access
+            return false;
+        }
+    };
+}
 const routeConfig: Routes = [
     {
         path: '',
@@ -15,12 +36,14 @@ const routeConfig: Routes = [
     {
         path: 'NewRequest',
         component: NewRequestComponent,
-        title: 'New Credit'
+        title: 'New Credit',
+        canActivate: [createCheckTokenAccess(false)]
     },
     {
         path: 'Requests',
         component: RequestsListComponent,
-        title: 'All Requests'
+        title: 'All Requests',
+        canActivate: [createCheckTokenAccess(false)]
     },
     {
         path: 'About',
